992cd1976c681ea30d8aedb4e01a0cb24528c3c0
unknown <Administrador@ORD-PROF-8.CUATROVIENTOS.local>
2015-09-24 14:30:49 +0200

    Initial commit

---
+++ app/src/main/java/org/cuatrovientos/divisor/MainActivity.java
@@ -0,0 +1,55 @@
+package org.cuatrovientos.divisor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import junit.framework.Test;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText editText1;
+    private EditText editText2;
+    private TextView result;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText1 = (EditText)findViewById(R.id.editText1);
+        editText2 = (EditText)findViewById(R.id.editText2);
+        result = (TextView)findViewById(R.id.result);
+    }
+    public void divide (View view) {
+        float a = 0;
+        float b = 0;
+        float division = 0;
+        a = Float.parseFloat(editText1.getText().toString());
+        b = Float.parseFloat(editText2.getText().toString());
+        division = a / b;
+        result.setText(division+"");
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
